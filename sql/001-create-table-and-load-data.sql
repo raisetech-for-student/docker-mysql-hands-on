3fcf3f833c775801b244764b0a0cc143ab0e725c
小山由人 <koyama@reytech.co.jp>
2023-02-07 13:16:19 +0900

    CSVロードするような仕組みを追加

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -9,3 +9,17 @@ CREATE TABLE movies (
 
 INSERT INTO movies (name, director) VALUES ("ショーシャンクの空に", "フランク・ダラボン");
 INSERT INTO movies (name, director) VALUES ("この世界の片隅に", "片渕須直");
+
+CREATE TABLE IF NOT EXISTS councilors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20),
+    commitee VARCHAR(15),
+    imagepath VARCHAR(30),
+    address VARCHAR(20),
+    tel_num VARCHAR(30),
+    birthday DATETIME,
+    homepage VARCHAR(50),
+    created_at DATETIME
+);
+
+LOAD DATA INFILE "/docker-entrypoint-initdb.d/002-councilors.csv" INTO TABLE councilors FIELDS TERMINATED BY ',';
